23a36dd148a619041c36655cf12f8b417558546c
PREM KUMAR MENNI <pr775390@dal.ca>
2019-10-25 20:58:21 -0300

    Adding Item to the bill

--- app/src/main/java/com/example/splinter/addBillActivity.java
+++ app/src/main/java/com/example/splinter/addBillActivity.java
@@ -6,10 +6,15 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.TextView;
 
 public class addBillActivity extends Activity {
+    private ArrayAdapter<String> itemsAdapter;
+    Button itemadd;
     EditText itemName, itemPrice, itemQuantity;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +24,27 @@ public class addBillActivity extends Activity {
         itemName = findViewById(R.id.editTextItemName);
         itemPrice = findViewById(R.id.editTextItemPrice);
         itemQuantity = findViewById(R.id.editTextItemQuantity);
+        itemadd = findViewById(R.id.btnAddItem);
+        ListView lvItems;
+        itemsAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1);
+        lvItems = findViewById(R.id.lv_item);
+        lvItems.setAdapter(itemsAdapter);
+
+        itemadd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String name, price, quantity;
+
+                name = itemName.getText().toString();
+                price = itemPrice.getText().toString();
+                quantity =itemQuantity.getText().toString();
+                itemsAdapter.add(name);
+                itemsAdapter.add(quantity);
+                itemsAdapter.add(price);
+            }
+        });
+
+
 
     }
 
