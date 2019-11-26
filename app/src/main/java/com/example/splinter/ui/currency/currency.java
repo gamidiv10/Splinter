119c7f0b4d4d2abbd5067d5d8de46f72f36db2cf
Prem Kumar <pr775390@dal.ca>
2019-11-25 22:48:53 -0400

    Home Screen and Menu Fragments Changes

---
+++ app/src/main/java/com/example/splinter/ui/currency/currency.java
@@ -0,0 +1,50 @@
+package com.example.splinter.ui.currency;
+
+import androidx.lifecycle.ViewModelProviders;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Spinner;
+
+import com.example.splinter.R;
+
+public class currency extends Fragment {
+
+    private ListView lvHomePage;
+    private String[] items;
+    private View view;
+    private CurrencyViewModel mViewModel;
+
+    private Spinner currencySpinner;
+    public static currency newInstance() {
+        return new currency();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.currency_fragment, container, false);
+        return view;
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        ArrayAdapter adapter = ArrayAdapter.createFromResource(getActivity(), R.array.currency,
+                android.R.layout.activity_list_item);
+
+    }
+
+
+}
--- app/src/main/java/com/example/splinter/ui/settings/SettingsViewModel.java
+++
@@ -1,7 +0,0 @@
-package com.example.splinter.ui.settings;
-
-import androidx.lifecycle.ViewModel;
-
-public class SettingsViewModel extends ViewModel {
-    // TODO: Implement the ViewModel
-}
